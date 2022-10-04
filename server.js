f8de27f80d523f25787b465ee5ca009e6c247313
Marcin Bieniek <marcingim@gmail.com>
2022-10-04 13:17:02 +0200

    Add contact form and connect CF with server

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './views/layouts', defaultLa
 app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.render('index', { layout: 'main' });
@@ -33,9 +34,22 @@ app.get('/hello/:name', (req, res) => {
     res.render('hello', {name: req.params.name, layout: 'main'})
 });
 
+app.post('/contact/send-message', (req, res) => {
+
+  const { author, sender, title, message } = req.body;
+
+  if(author && sender && title && message) {
+    res.send('The message has been sent!');
+  }
+  else {
+    res.send('You can\'t leave fields empty!')
+  }
+
+});
+
 app.use((req, res) => {
-    res.status(404).send('404 not found...');
-  })
+  res.status(404).send('404 not found...');
+})
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
